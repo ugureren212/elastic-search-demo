d48c89f052006ed9a61ed3b8ca1edff38e4431c7
Ugur Eren <ugureren212@gmail.com>
2024-12-20 15:05:10 +0000

    installed primevue finally...

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura'
+import 'primeicons/primeicons.css';
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.mount('#app');
